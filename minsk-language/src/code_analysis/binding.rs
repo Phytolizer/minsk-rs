01e7f6c12457ec6ac8a17e79fc3c6ed162bfc38e
onContentStop <mariorules24@gmail.com>
2020-12-05 22:25:12 -0600

    Refactor Evaluator

--- minsk-language/src/code_analysis/binding.rs
+++ minsk-language/src/code_analysis/binding.rs
@@ -1,11 +1,11 @@
 pub mod binder;
-mod bound_assignment_expression;
-mod bound_binary_expression;
+pub(super) mod bound_assignment_expression;
+pub(super) mod bound_binary_expression;
 mod bound_binary_operator;
 pub(super) mod bound_binary_operator_kind;
 pub(super) mod bound_expression;
-mod bound_literal_expression;
-mod bound_unary_expression;
+pub(super) mod bound_literal_expression;
+pub(super) mod bound_unary_expression;
 mod bound_unary_operator;
 pub(super) mod bound_unary_operator_kind;
-mod bound_variable_expression;
+pub(super) mod bound_variable_expression;
--- minsk-language/src/code_analysis/evaluator.rs
+++ minsk-language/src/code_analysis/evaluator.rs
@@ -1,9 +1,19 @@
 use std::collections::HashMap;
 
-use super::{minsk_value::MinskValue, variable_symbol::VariableSymbol};
+use super::{
+    binding::{
+        bound_assignment_expression::BoundAssignmentExpression,
+        bound_binary_expression::BoundBinaryExpression,
+        bound_unary_expression::BoundUnaryExpression,
+        bound_variable_expression::BoundVariableExpression,
+    },
+    minsk_value::MinskValue,
+    variable_symbol::VariableSymbol,
+};
 
 use super::binding::{
     bound_binary_operator_kind::BoundBinaryOperatorKind, bound_expression::BoundExpression,
+    bound_literal_expression::BoundLiteralExpression,
     bound_unary_operator_kind::BoundUnaryOperatorKind,
 };
 
@@ -22,62 +32,72 @@ impl<'compilation> Evaluator<'compilation> {
 
     fn evaluate_expression(&mut self, root: &BoundExpression) -> MinskValue {
         match root {
-            BoundExpression::Literal(lit) => lit.value.clone(),
-            BoundExpression::Unary(u) => {
-                let operand = self.evaluate_expression(&u.operand);
-                match u.op.kind {
-                    BoundUnaryOperatorKind::Identity => {
-                        MinskValue::Integer(operand.as_integer().unwrap())
-                    }
-                    BoundUnaryOperatorKind::Negation => {
-                        MinskValue::Integer(-operand.as_integer().unwrap())
-                    }
-                    BoundUnaryOperatorKind::LogicalNegation => {
-                        MinskValue::Boolean(!operand.as_boolean().unwrap())
-                    }
-                }
-            }
-            BoundExpression::Binary(b) => {
-                let left = self.evaluate_expression(&b.left);
-                let right = self.evaluate_expression(&b.right);
-                match b.op.kind {
-                    BoundBinaryOperatorKind::Addition => MinskValue::Integer(
-                        left.as_integer()
-                            .unwrap()
-                            .wrapping_add(right.as_integer().unwrap()),
-                    ),
-                    BoundBinaryOperatorKind::Subtraction => MinskValue::Integer(
-                        left.as_integer()
-                            .unwrap()
-                            .wrapping_sub(right.as_integer().unwrap()),
-                    ),
-                    BoundBinaryOperatorKind::Multiplication => MinskValue::Integer(
-                        left.as_integer()
-                            .unwrap()
-                            .wrapping_mul(right.as_integer().unwrap()),
-                    ),
-                    BoundBinaryOperatorKind::Division => MinskValue::Integer(
-                        left.as_integer()
-                            .unwrap()
-                            .wrapping_div(right.as_integer().unwrap()),
-                    ),
-                    BoundBinaryOperatorKind::Equality => MinskValue::Boolean(left == right),
-                    BoundBinaryOperatorKind::Inequality => MinskValue::Boolean(left != right),
-                    BoundBinaryOperatorKind::LogicalAnd => MinskValue::Boolean(
-                        left.as_boolean().unwrap() && right.as_boolean().unwrap(),
-                    ),
-                    BoundBinaryOperatorKind::LogicalOr => MinskValue::Boolean(
-                        left.as_boolean().unwrap() || right.as_boolean().unwrap(),
-                    ),
-                }
-            }
-            BoundExpression::Variable(v) => self.variables.get(&v.variable).unwrap().clone(),
-            BoundExpression::Assignment(a) => {
-                let value = self.evaluate_expression(&a.expression);
-                self.variables.insert(a.variable.clone(), value.clone());
-                value
+            BoundExpression::Literal(lit) => self.evaluate_literal_expression(lit),
+            BoundExpression::Unary(u) => self.evaluate_unary_expression(u),
+            BoundExpression::Binary(b) => self.evaluate_binary_expression(b),
+            BoundExpression::Variable(v) => self.evaluate_variable_expression(v),
+            BoundExpression::Assignment(a) => self.evaluate_assignment_expression(a),
+        }
+    }
+
+    fn evaluate_literal_expression(&mut self, lit: &BoundLiteralExpression) -> MinskValue {
+        lit.value.clone()
+    }
+
+    fn evaluate_unary_expression(&mut self, u: &BoundUnaryExpression) -> MinskValue {
+        let operand = self.evaluate_expression(&u.operand);
+        match u.op.kind {
+            BoundUnaryOperatorKind::Identity => MinskValue::Integer(operand.as_integer().unwrap()),
+            BoundUnaryOperatorKind::Negation => MinskValue::Integer(-operand.as_integer().unwrap()),
+            BoundUnaryOperatorKind::LogicalNegation => {
+                MinskValue::Boolean(!operand.as_boolean().unwrap())
             }
         }
+    }
+
+    fn evaluate_binary_expression(&mut self, b: &BoundBinaryExpression) -> MinskValue {
+        let left = self.evaluate_expression(&b.left);
+        let right = self.evaluate_expression(&b.right);
+        match b.op.kind {
+            BoundBinaryOperatorKind::Addition => MinskValue::Integer(
+                left.as_integer()
+                    .unwrap()
+                    .wrapping_add(right.as_integer().unwrap()),
+            ),
+            BoundBinaryOperatorKind::Subtraction => MinskValue::Integer(
+                left.as_integer()
+                    .unwrap()
+                    .wrapping_sub(right.as_integer().unwrap()),
+            ),
+            BoundBinaryOperatorKind::Multiplication => MinskValue::Integer(
+                left.as_integer()
+                    .unwrap()
+                    .wrapping_mul(right.as_integer().unwrap()),
+            ),
+            BoundBinaryOperatorKind::Division => MinskValue::Integer(
+                left.as_integer()
+                    .unwrap()
+                    .wrapping_div(right.as_integer().unwrap()),
+            ),
+            BoundBinaryOperatorKind::Equality => MinskValue::Boolean(left == right),
+            BoundBinaryOperatorKind::Inequality => MinskValue::Boolean(left != right),
+            BoundBinaryOperatorKind::LogicalAnd => {
+                MinskValue::Boolean(left.as_boolean().unwrap() && right.as_boolean().unwrap())
+            }
+            BoundBinaryOperatorKind::LogicalOr => {
+                MinskValue::Boolean(left.as_boolean().unwrap() || right.as_boolean().unwrap())
+            }
+        }
+    }
+
+    fn evaluate_variable_expression(&mut self, v: &BoundVariableExpression) -> MinskValue {
+        self.variables.get(&v.variable).unwrap().clone()
+    }
+
+    fn evaluate_assignment_expression(&mut self, a: &BoundAssignmentExpression) -> MinskValue {
+        let value = self.evaluate_expression(&a.expression);
+        self.variables.insert(a.variable.clone(), value.clone());
+        value
     }
 }
 
